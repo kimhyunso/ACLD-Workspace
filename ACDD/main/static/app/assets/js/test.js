7c5586182fa53cfec8d34da93bb6b56ea92dfb68
JaeWonSong <jwsonghub@gmail.com>
2023-06-08 17:32:09 +0900

    230608_17:32

--- ACDD/main/static/app/assets/js/test.js
+++
@@ -1,3 +0,0 @@
-function mybutton(){
-    alert('버튼을 눌렀습니다.');
-}
--- ACDD/main/views.py
+++ ACDD/main/views.py
@@ -3,21 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'app/home.html')
 
 
 def detail(request):
-    return render(request, 'detail.html')
+    return render(request, 'app/detail.html')
 
 
 
 def processing(request):
-    return render(request, 'processing.html')
+    return render(request, 'app/processing.html')
 
 
 def agree(request):
-    return render(request, 'agree.html')
+    return render(request, 'app/agree.html')
 
 
 def disagree(request):
-    return render(request, 'disagree.html')
+    return render(request, 'app/disagree.html')
