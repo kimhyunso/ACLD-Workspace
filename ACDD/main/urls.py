da9d95d13c6c281cc15ddc3c796181ecc915c798
Jae-Won_Song <jwsonghub@gmail.com>
2023-06-11 04:39:33 +0900

    230611_04:40

--- ACDD/main/urls.py
+++ ACDD/main/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('log/', views.log, name='log'),
     path('log_detail/', views.log_detail, name='log_detail'),
     path('report/', views.report, name='report'),
-    path('<int:page_no>/report_detail/', views.report_detail, name='report_detail'),
+    path('report_detail/', views.report_detail, name='report_detail'),
     path('chart/', views.chart, name='chart'),
     path('employee/', views.employee, name='employee'),
 ]
