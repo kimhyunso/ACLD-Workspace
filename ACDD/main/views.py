697cf53f4bcda8dd1ac8b5fc575d791744157e7c
kimhyunso <gustn9515@gmail.com>
2023-06-15 20:29:02 +0900

    20230615 20:29

--- ACDD/main/views.py
+++ ACDD/main/views.py
@@ -5,8 +5,6 @@ import json
 from .models import Agent, Dection, Report, identify, Department, Employee
 
 def home(request):
-    # Cam.objects.filter(name='홍길동')
-
     # # 'select * from cam where name = "홍길동"'
 
     # try:
@@ -24,22 +22,6 @@ def detail(request):
 def agent(request):
     return render(request, 'app/agent.html')
 
-# def log(request):
-#     return render(request, 'app/log.html')
-
-
-
-# def log_detail(request):
-#     return render(request, 'app/log_detail.html')
-
-
-# def report(request):
-#     return render(request, 'app/report.html')
-
-
-# def report_detail(request):
-#     return render(request, 'app/report_detail.html')
-
 def chart(request):
     return render(request, 'app/chart.html')
 
@@ -52,22 +34,18 @@ def addEmp(request):
         return render(request, 'app/addEmp.html')
     else:
         reqData = json.loads(request.body)
-        demp_name = reqData['demp_name']
         emp_name = reqData['emp_name']
         rank = reqData['rank']
         emp_no = reqData['emp_no']
         MAC = reqData['MAC']
         IP = reqData['IP']
         phone_no = reqData['phone_no']
-        landline = reqData['landline']
+        
         email = reqData['email']
-        location = reqData['location']
-        department = Department()
+        
         employee = Employee()
 
         department['depmt_name'] = demp_name
-        department['landline'] = landline
-        department['location'] = location
 
         employee['emp_no'] = emp_no
         employee['depmt_no'] = emp_no
@@ -92,6 +70,18 @@ def addDepart(request):
         return render(request, 'app/addDepart.html')
     else:
         reqData = json.loads(request.body)
+        landline = reqData['landline']
+        location = reqData['location']
+        demp_name = reqData['demp_name']
+
+        department = Department()
+        department.landline = landline
+        department.location = location
+        department.depmt_name = demp_name
+        department.save()
+        
+        return JsonResponse(reqData)
+
         
         
        
